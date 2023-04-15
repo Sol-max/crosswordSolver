fce92f3510db2c02f49db282b99db2eacab034e0
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-04-15 19:52:55 +0300

    Add files via upload

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,111 +1,61 @@
-// importing the necessary modules
-const readline = require('readline');
-const fs = require('fs');
-
-// modifying the function signature to include input file argument and output file argument
-function crosswordSolver(inputFile, outputFile) {
-
-  // reading the input file
-  const input = fs.readFileSync(inputFile, 'utf-8');
-
-  // splitting the input string on newlines and creating an array
-  const inputArray = input.split('\n');
-
-  // retrieving the size of the matrix from the first element of the array
-  const matrixSize = inputArray[0];
-
-  // retrieving the words to fill from the remaining elements of the array
-  const words = inputArray.slice(1, inputArray.length);
-
-  // an array in which we will construct the final crossword grid
-  const filledGrid = [];
-
-  // constructing a filledGrid full of spaces (empty)
-  for (let i = 0; i < matrixSize; i += 1) {
-    filledGrid.push([]);
-    for (let j = 0; j < matrixSize; j += 1) {
-      filledGrid[i].push(' ');
-    }
-  }
-
-  // getting the empty spaces in the crossword and adding their x, y coordinates and direction (horizontal/vertical) to an array
-  const emptySpaces = [];
-
-  for (let i = 0; i < matrixSize; i += 1) {
-    for (let j = 0; j < matrixSize; j += 1) {
-      // if the current slot is a number, it's the beginning of a new word, so add it
-      if (inputArray[i][j] !== '.') {
-        if (i + 1 < matrixSize && inputArray[i + 1][j] !== '.') {
-          // vertical word here, so we want to include everything from here down
-          let wordLength = 1;
-          while (i + wordLength < matrixSize && inputArray[i + wordLength][j] !== '.') {
-            wordLength += 1;
+function crosswordSolver(puzzle, words) {
+    // Convert the puzzle string to an array of arrays
+    const puzzleArray = puzzle.split('\n').map(row => row.split(''));
+  
+    // Create an object to keep track of the number of words that start from each position
+    const wordCounts = {};
+  
+    for (let row = 0; row < puzzleArray.length; row++) {
+      for (let col = 0; col < puzzleArray[0].length; col++) {
+        const currentCell = puzzleArray[row][col];
+  
+        // If the current cell is not blocked and is the start of a word, increment the word count
+        if (currentCell !== '.' && currentCell !== '-') {
+          if (puzzleArray[row - 1]?.[col] === '-' || puzzleArray[row]?.[col - 1] === '-') {
+            const positionKey = `${row},${col}`;
+            wordCounts[positionKey] = parseInt(currentCell);
           }
-          emptySpaces.push({
-            x: i,
-            y: j,
-            length: wordLength,
-            direction: 'down',
-          });
-        }
-        if (j + 1 < matrixSize && inputArray[i][j + 1] !== '.') {
-          // horizontal word here, so we want to include everything from here right
-          let wordLength = 1;
-          while (j + wordLength < matrixSize && inputArray[i][j + wordLength] !== '.') {
-            wordLength += 1;
-          }
-          emptySpaces.push({
-            x: i,
-            y: j,
-            length: wordLength,
-            direction: 'across',
-          });
         }
       }
     }
-  }
-
-  // sorting the empty spaces by length (in descending order)
-  emptySpaces.sort((a, b) => b.length - a.length);
-
-  // creating an array of all possible word placements
-  const possiblePlacements = [];
-
-  // going through each empty space
-  for (let i = 0; i < emptySpaces.length; i += 1) {
-    const { x, y, length, direction } = emptySpaces[i];
-
-// getting all the words the right length that can fit here
-const possibleWords = [];
-for (let j = 0; j < words.length; j += 1) {
-  if (words[j].length === length) {
-    if (direction === 'across') {
-      if (y + length <= matrixSize) {
-        let isPossible = true;
-        for (let k = 0; k < length; k += 1) {
-          if (filledGrid[x][y + k] !== ' ' && filledGrid[x][y + k] !== words[j][k]) {
-            isPossible = false;
-            break;
+  
+    // Iterate through the list of words and try to place each one in the puzzle
+    for (let word of words) {
+      let wordPlaced = false;
+  
+      for (let row = 0; row < puzzleArray.length && !wordPlaced; row++) {
+        for (let col = 0; col < puzzleArray[0].length && !wordPlaced; col++) {
+          if (!puzzleArray[row][col] ||
+              (puzzleArray[row][col] === '-' && (puzzleArray[row - 1]?.[col] !== '-' && puzzleArray[row][col - 1] !== '-'))) {
+            if (tryWord(word, row, col, 'across') || tryWord(word, row, col, 'down')) {
+              wordPlaced = true;
+              const positionKey = `${row},${col}`;
+              wordCounts[positionKey]--;
+  
+              if (wordCounts[positionKey] === 0) {
+                delete wordCounts[positionKey];
+              }
+            }
           }
-        }
-        if (isPossible) {
-          possibleWords.push(words[j]);
         }
       }
-    } else {
-      if (x + length <= matrixSize) {
-        let isPossible = true;
-        for (let k = 0; k < length; k += 1) {
-          if (filledGrid[x + k][y] !== ' ' && filledGrid[x + k][y] !== words[j][k]) {
-            isPossible = false;
-            break;
-          }
-        }
-        if (isPossible) {
-          possibleWords.push(words[j]);
-        }
+  
+      // If a word couldn't be placed, print an error message and return
+      if (!wordPlaced) {
+        return 'Error';
       }
     }
+  
+    // Fill in any remaining empty cells with a period
+    puzzleArray.forEach(row => row.forEach((cell, index) => {
+      if (!cell) {
+        row[index] = '.';
+      }
+    }));
+  
+    // Convert the puzzle array back to a string and return it
+    return puzzleArray.map(row => row.join('')).join('\n');
   }
-}
-  }}
+  
+  module.exports = crosswordSolver;
+    
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const crosswordSolver  = require('./crosswordSolver.js');
 
-
-const emptyPuzzle = 
-"2001\n0..0\n1000\n0..0";
+const emptyPuzzle = `2001
+0..0
+1000
+0..0`
+// const emptyPuzzle = 
+// "2001\n0..0\n1000\n0..0";
 
 const words = ['casa', 'alan', 'ciao', 'anta'];
 
