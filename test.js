b7b3b33c6cbd498029fd23866b6fc2d354629813
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-04-16 00:30:20 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,21 +1,17 @@
-const  {crosswordSolver, tryWord} = require('./crosswordSolver');
-
+const { crosswordSolver, printBoard } = require("./crosswordSolver");
 
 const emptyPuzzle = `2001
 0..0
 1000
 0..0`;
 
-// const puzzleArray = emptyPuzzle.split('\n').map(row => row.split(''));
+const words = ["casa", "alan", "ciao", "anta"];
 
+// solve the crossword puzzle and get the board, rows, and cols as an object
+const crosswordSolution = crosswordSolver(emptyPuzzle, words);
 
-const words = ['casa', 'alan', 'ciao', 'anta'];
-
-// solve the crossword puzzle
-const solution = crosswordSolver(emptyPuzzle, words); // crosswordSolver(puzzleArray, wordList);
-
-if (solution === 'Error') {
-  console.log('The puzzle cannot be solved');
+if (crosswordSolution === "Error") {
+  console.log("The puzzle cannot be solved");
 } else {
-  console.log(solution);
+  printBoard(crosswordSolution); // pass the crosswordSolution object to printBoard
 }
