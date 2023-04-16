02e0ccb7ee62a01e0a5e47ce567dd1de14d998f7
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-04-16 11:38:05 +0300

    Add files via upload

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -4,36 +4,27 @@ function crosswordSolver(puzzleString, words) {
 
   // Recursively try to fill in the blank spaces with words
   function tryFill(puzzle, blankIndex) {
+    console.log('puzzle:', puzzle);
+    console.log('blankIndex:', blankIndex);
+    console.log('blankPositions:', blankPositions);
     // If we've filled in all the blank spaces, return the filled puzzle
     if (blankIndex === blankPositions.length) {
       return puzzle.map(row => row.join('')).join('\n');
     }
-  
+
     const [i, j] = blankPositions[blankIndex];
     for (let word of words) {
       if (tryWord(puzzle, word, i, j)) {
-        const newPuzzle = puzzle.map(row => [...row]);
-        // If the word fits horizontally, add it horizontally
-        if (j + word.length <= puzzle[i].length) {
-          newPuzzle[i].splice(j, word.length, ...word.split(''));
-        }
-        // If the word fits vertically, add it vertically
-        else if (i + word.length <= puzzle.length) {
-          for (let index = 0; index < word.length; index++) {
-            newPuzzle[i + index][j] = word[index];
-          }
-        }
-        const filledPuzzle = tryFill(newPuzzle, blankIndex + 1);
-        if (filledPuzzle !== null) {
-          return filledPuzzle;
+        const newPuzzle = tryFill(puzzle, blankIndex + 1);
+        if (newPuzzle !== null) {
+          return newPuzzle;
         }
       }
     }
-  
+
     // If we've tried all the words and none of them worked, return null
     return null;
   }
-  
 
   // Find the positions of the blank spaces in the puzzle
   const blankPositions = [];
@@ -53,26 +44,29 @@ function crosswordSolver(puzzleString, words) {
   return filledPuzzle;
 }
 
-// Check if a word can be placed in a specific position in the puzzle
+
 function tryWord(puzzle, word, i, j) {
+  console.log("tryWord: ", word, i, j);
+  const clonePuzzle = JSON.parse(JSON.stringify(puzzle));
+  let skip = false;
   // Check if the word fits horizontally
-  if (j + word.length <= puzzle[i].length && puzzle[i].slice(j, j + word.length).every((letter, index) => letter === '.' || letter === word[index])) {
-    const newPuzzle = puzzle.map(row => [...row]);
+  if (j + word.length <= clonePuzzle[i].length && clonePuzzle[i].slice(j, j + word.length).every((letter, index) => letter === '.' || letter === word[index])) {
+    const newPuzzle = clonePuzzle.map(row => [...row]);
     newPuzzle[i].splice(j, word.length, ...word.split(''));
     return newPuzzle;
   }
 
   // Check if the word fits vertically
-  if (i + word.length <= puzzle.length && puzzle.slice(i, i + word.length).map(row => row[j]).every((letter, index) => letter === '.' || letter === word[index])) {
-    const newPuzzle = puzzle.map(row => [...row]);
+  if (i + word.length <= clonePuzzle.length && clonePuzzle.slice(i, i + word.length).map(row => row[j]).every((letter, index) => letter === '.' || letter === word[index])) {
+    const newPuzzle = clonePuzzle.map(row => [...row]);
     for (let index = 0; index < word.length; index++) {
       newPuzzle[i + index][j] = word[index];
     }
     return newPuzzle;
   }
 
-  // If the word doesn't fit horizontally or vertically, return false
-  return false;
+  // If the word doesn't fit horizontally or vertically, return null
+  return null;
 }
 
 
